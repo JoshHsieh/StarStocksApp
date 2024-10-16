1d90f12c1a1d2d12f19fa94f0bdd3fc2eab6bf02
josh <josh@octalord.com>
2024-10-16 17:42:32 +0800

    feat: add functional extensions: string, guid, enum, convert function

--- StarStocks.Core/Extensions/ConvertExtensions.cs
+++ StarStocks.Core/Extensions/ConvertExtensions.cs
@@ -4,7 +4,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace StarStocks.Core.Extensions
 {
@@ -41,6 +43,115 @@ namespace StarStocks.Core.Extensions
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// bool to int value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int BoolToInt(this bool value)
+        {
+            return value ? 1 : 0;
+        }
+
+        /// <summary>
+        /// JObject to nested dictionaries
+        /// </summary>
+        /// <param name="jobject"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> ToDictionary(this JObject jobject)
+        {
+            var dico = new Dictionary<string, object>();
+
+            foreach (var o in jobject)
+            {
+                var oValue = o.Value;
+
+                var jArray = o.Value as JArray;
+                if (jArray != null)
+                {
+                    var first = jArray[0]; // use first element to guess if we are dealing with a list of dico or an array
+                    var isValueArray = first is JValue;
+
+                    if (isValueArray)
+                    {
+                        var array = jArray.Values().Select(x => ((JValue)x).Value).ToArray();
+                        dico[o.Key] = array;
+                    }
+                    else
+                    {
+                        var list = new List<IDictionary<string, object>>();
+                        foreach (var token in jArray)
+                        {
+                            var elt = ((JObject)token).ToDictionary();
+                            list.Add(elt);
+                        }
+
+                        dico[o.Key] = list;
+                    }
+                }
+                else
+                {
+                    dico[o.Key] = ((JValue)oValue).Value;
+                }
+            }
+
+            return dico;
+        }
+
+        /// <summary>
+        /// convert list to string with quote & comma
+        /// usage : string xxx = string.Format("select * from xxx where id in ({0});", list.ToSplitString(null));
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="cast"></param>
+        /// <returns></returns>
+        public static string ToSplitString<TSource>(this IEnumerable<TSource> source, Func<TSource, string> cast)
+        {
+            const string Empty = "''";
+
+            if (source == null)
+                return Empty;
+
+            var sb = new StringBuilder();
+
+            foreach (var entity in source)
+            {
+                if (cast == null)
+                    sb.Append(",'").Append(entity.ToSqlQuoteString()).Append("'");
+                else
+                    sb.Append(",'").Append(cast(entity).ToSqlQuoteString()).Append("'");
+            }
+
+            if (sb.Length == 0)
+                return Empty;
+
+            return sb.ToString().Substring(1);
+        }
+
+        /// <summary>
+        /// convert object to string with quote mark
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToSqlQuoteString(this object obj)
+        {
+            string str = obj.ToStringNotNull();
+            return str.Replace("'", "''");
+        }
+
+        /// <summary>
+        /// convert object to string, if object is null return empty string
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToStringNotNull(this object obj)
+        {
+            if (obj == null) return string.Empty;
+            if (obj == DBNull.Value) return string.Empty;
+            return obj.ToString().Trim();
         }
     }
 }
---
+++ StarStocks.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,171 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace StarStocks.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// http://stackoverflow.com/questions/4367723/get-enum-from-description-attribute
+        /// </summary>
+        /// <param name="value">Enum to get description</param>
+        /// <returns></returns>
+        public static string GetAttribute(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+
+            var attribute
+                    = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute))
+                        as DescriptionAttribute;
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+
+        /// <summary>
+        /// 将枚举类型转换为 int ， 返回 int值的string 形式
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string EnumToIntString(this object obj)
+        {
+            return ((int)obj).ToString();
+        }
+
+        /// <summary>
+        /// Gets all items for an enum value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAllItems<T>(this Enum value)
+        {
+            foreach (object item in Enum.GetValues(typeof(T)))
+            {
+                yield return (T)item;
+            }
+        }
+
+        /// <summary>
+        /// Gets all items for an enum type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAllItems<T>() where T : struct
+        {
+            foreach (object item in Enum.GetValues(typeof(T)))
+            {
+                yield return (T)item;
+            }
+        }
+
+        /// <summary>
+        /// Gets all combined items from an enum value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        /// <example>
+        /// Displays ValueA and ValueB.
+        /// <code>
+        /// EnumExample dummy = EnumExample.Combi;
+        /// foreach (var item in dummy.GetAllSelectedItems<EnumExample>())
+        /// {
+        ///    Console.WriteLine(item);
+        /// }
+        /// </code>
+        /// </example>
+        public static IEnumerable<T> GetAllSelectedItems<T>(this Enum value)
+        {
+            int valueAsInt = Convert.ToInt32(value, CultureInfo.InvariantCulture);
+
+            foreach (object item in Enum.GetValues(typeof(T)))
+            {
+                int itemAsInt = Convert.ToInt32(item, CultureInfo.InvariantCulture);
+
+                if (itemAsInt == (valueAsInt & itemAsInt))
+                {
+                    yield return (T)item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the enum value contains a specific value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="request">The request.</param>
+        /// <returns>
+        ///     <c>true</c> if value contains the specified value; otherwise, <c>false</c>.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// EnumExample dummy = EnumExample.Combi;
+        /// if (dummy.Contains<EnumExample>(EnumExample.ValueA))
+        /// {
+        ///     Console.WriteLine("dummy contains EnumExample.ValueA");
+        /// }
+        /// </code>
+        /// </example>
+        public static bool Contains<T>(this Enum value, T request)
+        {
+            int valueAsInt = Convert.ToInt32(value, CultureInfo.InvariantCulture);
+            int requestAsInt = Convert.ToInt32(request, CultureInfo.InvariantCulture);
+
+            if (requestAsInt == (valueAsInt & requestAsInt))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets an attribute on an enum field value
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
+        /// <param name="enumVal">The enum value</param>
+        /// <returns>The attribute of type T that exists on the enum value</returns>
+        /// <example>string desc = myEnumVariable.GetAttributeOfType<DescriptionAttribute>().Description;</example>
+        public static T GetAttribute<T>(this Enum enumVal) where T : System.Attribute
+        {
+            var type = enumVal.GetType();
+
+            var memInfo = type.GetMember(enumVal.ToString());
+
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+
+        public static string ToName(this Enum value)
+        {
+            var attribute = value.GetAttribute<DescriptionAttribute>();
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+
+        public static string ToEnumString<TEnum>(this int enumValue)
+        {
+            var enumString = enumValue.ToString();
+
+            if (Enum.IsDefined(typeof(TEnum), enumValue))
+            {
+                enumString = ((TEnum)Enum.ToObject(typeof(TEnum), enumValue)).ToString();
+            }
+            return enumString;
+        }
+
+        public static T ToEnum<T>(this string enumString)
+        {
+            return (T)Enum.Parse(typeof(T), enumString);
+        }
+    }
+}
